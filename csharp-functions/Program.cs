8a8b6c03111a3451d90e5ec72ffa4e60763ee275
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-07 13:58:35 +0200

    completate le funzioni generali

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// FUNZIONI
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        else
+        {
+            Console.Write(array[i]);
+        }
+    }
+
+    Console.WriteLine("]");
+}
+
+int Quadrato(int numero)
+{
+    return numero * numero;
+}
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] arrayCopy = (int[])array.Clone();
+
+    for (int i = 0;i < arrayCopy.Length;i++)
+    {
+        arrayCopy[i] = Quadrato(arrayCopy[i]);
+    }
+
+    return arrayCopy;
+}
+
+int sommaElementiArray(int[] array)
+{
+    int somma = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        somma += array[i];
+    }
+
+    return somma;
+}
